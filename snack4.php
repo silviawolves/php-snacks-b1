9bf59556d950676441b3760edfb5243c31a3c979
silviawolves <silviacanepa.w@gmail.com>
2022-07-08 14:27:51 +0200

    snack4 - completo

--- snack4.php
+++ snack4.php
@@ -1,7 +1,18 @@
+<?php
 
+$arrayNumeriCasuali = [];
 
+for ($i = 0; $i < 15; $i++) {
+    $numeroCasuale = rand(1,100);
+    if (!in_array($numeroCasuale ,$arrayNumeriCasuali)) {
+        $arrayNumeriCasuali[] = $numeroCasuale;
+    } else {
+        $i--;
+    }
+}
+var_dump($arrayNumeriCasuali);
 
-
+?>
 
 <!-- ## Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
